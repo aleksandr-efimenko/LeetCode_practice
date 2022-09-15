595f97c34dd726ea9e4af4ef4e57afbbc7470857
androidblog <androidblog@yandex.ru>
2022-09-15 12:11:43 +0400

    Problem 2007 solved

--- find_original_array_from_doubled_array_2007.py
+++ find_original_array_from_doubled_array_2007.py
@@ -1,4 +1,29 @@
 from typing import List
 
 def findOriginalArray(changed: List[int])-> List[int]:
-    pass
+    if len(changed) % 2 != 0:
+        return []
+    original = []
+    counted_nums = {}
+    changed.sort()
+    for num in changed:
+        counted_nums[num] = counted_nums.get(num, 0) + 1
+    print(counted_nums)
+    if counted_nums.get(0) is not None:
+        if counted_nums.get(0) % 2 == 0:
+            original = [0] * int(counted_nums.get(0) / 2)
+        else:
+            return []
+
+    for key, value in counted_nums.items():
+        if counted_nums.get(key * 2, 0) >= counted_nums.get(key) or counted_nums.get(key) == 0:
+            if counted_nums.get(key) == 0:
+                continue
+            counted_nums[key * 2] = counted_nums[key * 2] - counted_nums[key]
+            original = original + [key] * counted_nums[key]
+            counted_nums[key] = 0
+        else:
+            return []
+    return original
+
+print(findOriginalArray([1,3,3,6,2,4,2,6,8]))
--- test_find_original_array_from_doubled_array_2007.py
+++ test_find_original_array_from_doubled_array_2007.py
@@ -9,6 +9,7 @@ class FOA_Case(unittest.TestCase):
     def test_findOriginalArray(self):
         cases = [
             ([1,3,4,2,6,8], [1,3,4]),
+            ([0,0,0,0], [0,0]),
             ([6,3,0,1], []),
             ([1],[])
         ]
